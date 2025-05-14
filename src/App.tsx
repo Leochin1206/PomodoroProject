df3237988e91f4449d3821d15a39ae3c3a71995f
Leochin1206 <leoaviana1206@gmail.com>
2025-05-13 21:25:31 -0300

    13 05 2025

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./styles/theme.css";
+import "./styles/global.css";
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
+
+export function App() {
+  return (
+    <>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
+    </>
+  );
+}
