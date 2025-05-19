c6c319e5805fb63706961fa1490426b88d81ad63
Leochin1206 <leoaviana1206@gmail.com>
2025-05-18 21:17:19 -0300

    18 05 25

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
+
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Container } from "./components/Container";
-import { Heading } from "./components/Heading";
+import { CountDown } from "./components/CountDown";
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
 
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" accessKey="">
+          <div className="formRow">
+            <label htmlFor="meuInput">task</label>
+            <input id="meuInput" type="text" />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
